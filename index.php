4d7d2d0ee7a89eed76d3cded60fef9030d56cd42
Tihomirov <tihomirov@meiji.media>
2021-08-06 06:40:27 +0300

    #complex #math #complexmath #interface. индексный файл.

--- index.php
+++ index.php
@@ -10,18 +10,22 @@ echo '<div>';
 $cA = new ComplexNumberAlgebraic(['r' => 2, 'i' => 1]);
 $cB = new ComplexNumberAlgebraic([3, 4]);
 
-$cA->getType();
+outParagraph($cA->getType());
 
 outParagraph('комплексные числа:');
 outParagraph('Z1 = ' . $cA . ', Z2 = ' . $cB);
 outParagraph('Результат сложения: ' . CMath::add($cA, $cB));
 
-$cA = new ComplexNumberTrigonometric(['m' => 20, 'a' => 3.141592654 / 2]);
-$cB = new ComplexNumberTrigonometric([15, 3.141592654]);
+$cC = new ComplexNumberTrigonometric(['m' => 20, 'a' => 3.141592654 / 2]);
+$cD = new ComplexNumberTrigonometric([15, 3.141592654]);
+
+outParagraph('Z1 = ' . $cC . ', Z2 = ' . $cD);
+outParagraph('inverse: ' . $cC->inverse());
+outParagraph('Результат вычитания: ' . CMath::subtract($cC, $cD));
 
 outParagraph('комплексные числа:');
 outParagraph('Z1 = ' . $cA . ', Z2 = ' . $cB);
-outParagraph('Результат сложения: ' . CMath::add($cA, $cB));
+outParagraph('Результат вычитания: ' . CMath::subtract($cA, $cB));
 
 $cA = new ComplexNumberTrigonometric(['m' => 15, 'a' => 3.141592654 * 2]);
 $cB = new ComplexNumberAlgebraic([10, 20]);
@@ -29,6 +33,16 @@ $cB = new ComplexNumberAlgebraic([10, 20]);
 outParagraph('комплексные числа:');
 outParagraph('Z1 = ' . $cA . ', Z2 = ' . $cB);
 outParagraph('Результат сложения: ' . $cA->add($cB));
+
+$cA = new ComplexNumberAlgebraic(['r'=>-5, 'i'=>5]);
+$cB = new ComplexNumberAlgebraic(['r' => 10, 'i' => -10]);
+
+$cA = new ComplexNumberAlgebraic('1+1i');
+$cB = new ComplexNumberAlgebraic('-2-2i');
+
+outParagraph('комплексные числа:');
+outParagraph('Z1 = ' . $cA . ', Z2 = ' . $cB);
+outParagraph('Результат умножения: ' . $cA->divide($cB));
 
 echo '</div>';
 
